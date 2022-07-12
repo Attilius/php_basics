dc987a00d1028a63742ea7f582f206b3a8e5990d
Attilius <leo20@freemail.hu>
2022-07-12 20:49:47 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once "core/config.php";
+require_once "core/functions.php";
+require_once "core/app.php";
+require_once "templates/layout.php";
+
+?>
