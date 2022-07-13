c763145f4e1e8542e20d9da47e5c6bd56eb42137
Attilius <leo20@freemail.hu>
2022-07-13 15:12:01 +0200

    Modified single.php

--- templates/single.php
+++ templates/single.php
@@ -1,17 +1,17 @@
 <main class="container mt-5">
     <div class="row">
         <div class="col-md-6">
-            <img width="100%" class="mt-2 mb-2 mx-1" title="<?= $image['title']; ?>" src="<?= $image['url']; ?>" />
+            <img width="100%" class="mt-2 mb-2 mx-1" title="<?php esc( $image['title'] ); ?>" src="<?php esc( $image['url'] ); ?>" />
         </div>
         <div class="col-md-6">
-            <form method="post" action="/php_basics/image/<?php echo $image['id'] ?>/edit">
+            <form method="post" action="/php_basics/image/<?php esc( $image['id'] ); ?>/edit">
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input id="title" value="<?php echo $image['title'] ?>" class="form-control" placeholder="Enter the title here." />
+                    <input id="title" value="<?php esc( $image['title'] ); ?>" class="form-control" placeholder="Enter the title here." />
                 </div>
                 <button type="submit" class="btn btn-primary">Update</button>
             </form>
-            <form class="mt-5" method="post" action="/php_basics/image/<?php echo $image['id'] ?>/delete">
+            <form class="mt-5" method="post" action="/php_basics/image/<?php esc( $image['id'] ); ?>/delete">
                 <div class="form-group">
                     <label for="title">Danger zone</label>
                 </div>
