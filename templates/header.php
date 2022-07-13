c0a78037bf6467d9dc7eaf8e09efc45f39a9f1d1
Attilius <leo20@freemail.hu>
2022-07-13 15:09:30 +0200

    Modified headers.php

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,13 @@
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
     <a class="navbar-brand mx-2" href="/php_basics/">My images</a>
+    <ul class="navbar-nav pull-right">
+        <li class="nav-item active">
+            <?php if ($loggedIn):?>
+            <a class="nav-link" href="/php_basics/logout">Logout</a>
+            <?php else:?>
+            <a class="nav-link" href="/php_basics/login">Login</a>
+            <?php endif; ?>
+        </li>
+    </ul>
 </nav>
